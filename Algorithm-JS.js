1d2e91381d540ed8d13ed49a0badc8ce801aebd0
WonseoYang <dnjstj96@gmail.com>
2021-06-27 15:35:03 +0900

    for in => forEach문으로 변경하여 몇몇 구문 단순화

--- Algorithm-JS.js
+++ Algorithm-JS.js
@@ -1,30 +1,42 @@
-// let param0 = ["/a/a_v2.x",
+/* jshint esversion: 6*/
 
-// "/b/a.x",
-// "/c/t.z",
-// "/d/a/t.x",
-// "/e/z/t_v1.z",
-// "/k/k/k/a_v9.x"];
+let param0 = ["/a/a_v2.x",
 
-let param0 = ["/t.z",
-"/b/b.z",
-"/a.z",
-"/e/k.z",
-"/d/a/x_v2.z"];
+"/b/a.x",
+"/c/t.z",
+"/d/a/t.x",
+"/e/z/t_v1.z",
+"/k/k/k/a_v9.x"];
+
+// let param0 = ["/t.z",
+// "/b/b.z",
+// "/a.z",
+// "/e/k.z",
+// "/d/a/x_v2.z"];
 
 function solution(param0) {
 
     let fileList = [];//앞으로의 파일명을 저장할 배열 변수
 
-    for(key in param0){//돌며 디렉토리 명들을 분류하여 끝에 파일 명만 파일명 배열 변수에 저장
-        fileList[key] = param0[key].split("/").pop();
-        //중간 버전 명 존재하면 제거
-        if(fileList[key].slice(fileList[key].indexOf("_"), fileList[key].indexOf("."))){
-            let [fileFixedName, fixedFileName2] = fileList[key].split(fileList[key].slice(fileList[key].indexOf("_"), fileList[key].indexOf(".")));
-            fixedFileName = fileFixedName + fixedFileName2;
-            fileList[key] = fixedFileName;
+    // for(key in param0){//돌며 디렉토리 명들을 분류하여 끝에 파일 명만 파일명 배열 변수에 저장
+    //     fileList[key] = param0[key].split("/").pop();
+    //     //중간 버전 명 존재하면 제거
+    //     if(fileList[key].slice(fileList[key].indexOf("_"), fileList[key].indexOf("."))){
+    //         let [fileFixedName, fixedFileName2] = fileList[key].split(fileList[key].slice(fileList[key].indexOf("_"), fileList[key].indexOf(".")));
+    //         fixedFileName = fileFixedName + fixedFileName2;
+    //         fileList[key] = fixedFileName;
+    //     }
+    // }
+
+    param0.forEach((item, index)=>{
+        fileList[index] = item.split("/").pop();
+        if(fileList[index].slice(fileList[index].indexOf("_"), fileList[index].indexOf("."))){
+                    let [fileFixedName, fixedFileName2] = fileList[index].split(fileList[index].slice(fileList[index].indexOf("_"), fileList[index].indexOf(".")));
+                    fixedFileName = fileFixedName + fixedFileName2;
+                    fileList[index] = fixedFileName;
         }
-    }
+    });
+
     fileList.sort((a,b)=>a-b);
     const answer = [];
     let cnt;
@@ -39,6 +51,6 @@ function solution(param0) {
     
     return answer.length == 0 ? [] : answer;
 
-};
+}
 
 console.log(solution(param0));
