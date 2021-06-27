a8ceab5f8f57511b295be1ab38b2d5e17e9bf575
WonseoYang <dnjstj96@gmail.com>
2021-06-27 15:45:53 +0900

    2021부캠 자가진단 6번(알고리즘)

---
+++ boostcamp_ct_SelfTest.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,3,3,3,4,4];
+        const answer = [];
+        
+        function solution(arr) {
+            const arrEx = new Array(Math.max(...arr)).fill(0);
+
+            for(let i=0; i<arr.length; i++){
+                arrEx[arr[i]-1]++;
+            };
+
+            const result = arrEx.filter(item => item>1);
+
+            return arrEx.length == 0 ? -1 : result ;
+        }
+
+        console.log(solution(arr));
