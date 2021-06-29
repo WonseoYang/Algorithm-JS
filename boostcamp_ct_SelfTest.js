03a10306fae8c5b7c275d4ac456a9bf7f741134d
WonseoYang <dnjstj96@gmail.com>
2021-06-30 00:17:27 +0900

    나머지 매개변수 대신 prototype으로 변경해보기
    이전 대비 가장 빠른 기록 기준 0.05초 가량 빨라짐

--- boostcamp_ct_SelfTest.js
+++ boostcamp_ct_SelfTest.js
@@ -2,7 +2,7 @@ const arr = [1,2,3,3,3,3,4,4];
         const answer = [];
         
         function solution(arr) {
-            const arrEx = new Array(Math.max(...arr)).fill(0);
+            const arrEx = new Array(Math.max.apply(null, arr)).fill(0);
 
             for(let i=0; i<arr.length; i++){
                 arrEx[arr[i]-1]++;
