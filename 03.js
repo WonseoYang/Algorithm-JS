a6a9e74e725274930de95d6ec1d924d8f8267f36
WonseoYang <dnjstj96@gmail.com>
2021-07-12 17:32:38 +0900

    03
    prettier 적용

--- 03.js
+++ 03.js
@@ -1,28 +1,27 @@
-/* jshint esversion: 6*/
+/* jshint esversion: 6 */
 
-//case 1
-let participant = ["leo", "kiki", "eden"];
-let completion = ["eden", "kiki"];
+// case 1
+let participant = ['leo', 'kiki', 'eden']
+let completion = ['eden', 'kiki']
 
-//case 2
+// case 2
 // let participant = ["marina", "josipa", "nikola", "vinko", "filipa"];
 // let completion = ["josipa", "filipa", "marina", "nikola"];
 
-//case 3
+// case 3
 // let participant = ["mislav", "stanko", "mislav", "ana"];
 // let completion = ["stanko", "ana", "mislav"];
 
+function solution (participant, completion) {
+  participant.sort()
+  completion.sort()
 
-function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-
-    for(let i=0; i<participant.length;i++){
-        if(participant[i] !== completion[i]){
-            const answer = participant[i];
-            return answer;
-        }
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      const answer = participant[i]
+      return answer
     }
-};
+  }
+}
 
-console.log(solution(participant, completion));
+console.log(solution(participant, completion))
